58a0817a84c001157712a86a816441ef17f29b78
Oz <shinhaaa22@gmail.com>
2020-09-04 11:25:23 +0900

    o-shin1ファーストコミット

--- resources/views/aaa.blade.php
+++ resources/views/aaa.blade.php
@@ -84,7 +84,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    aaaaaaa
+                    bbbbbbb
                 </div>
 
                 <div class="links">
