44a708a3eccada6429b0fe9fa88a6fb597ace927
ryusei <perusonaryu1127@gmail.com>
2020-10-12 22:05:57 +0900

    bbb

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -25,6 +25,8 @@ window.axios = require('axios');
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
+window.axios.defaults.baseURL = process.env.MIX_SENTRY_DSN_PUBLIC;
+
 /**
  * Echo exposes an expressive API for subscribing to channels and listening
  * for events that are broadcast by Laravel. Echo and event broadcasting
